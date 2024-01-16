3884539175d0429efd81f1041ffbcf82e71e127a
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2024-01-16 13:12:52 -0500

    viteClean-n-signalIn

---
+++ src/LogoLoader.jsx
@@ -0,0 +1,12 @@
+const LogoLoader = () => {
+    return (
+      <div>
+        <p>a</p>
+        <p>b</p>
+        <p>c</p>
+        <p>struktura</p>
+      </div>
+    );
+  };
+  
+  export default LogoLoader;
