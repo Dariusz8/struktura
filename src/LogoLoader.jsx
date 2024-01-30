115fcad22475e7ca750f148f18ba69199c4e514d
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2024-01-29 20:26:15 -0500

    cloudinary test1

--- src/LogoLoader.jsx
+++ src/LogoLoader.jsx
@@ -1,8 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import './assets/LogoLoader.css'
+import './assets/LogoLoader.css';
+import { Cloudinary } from '@cloudinary/url-gen';
+import {AdvancedImage} from '@cloudinary/react';
+import {fill} from "@cloudinary/url-gen/actions/resize";
 
 const LogoLoader = () => {
   const [isVisible, setIsVisible] = useState(false);
+  //create and configure to my cloudinary instance
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'dly85se71'
+    }
+  });
+
+  const myImage1 = cld.image('STRUKTURA1');
+  const myImage2 = cld.image('STRUKTURA2');
+  const myImage3 = cld.image('STRUKTURA3');
+  const myImage4 = cld.image('STRUKTURA4');
+  
+  myImage1.resize(fill().width(200).height(200));
+  myImage2.resize(fill().width(200).height(200));
+  myImage3.resize(fill().width(200).height(200));
+  myImage4.resize(fill().width(1400).height(200));
 
   useEffect(() => {
     const delay = 1000;
@@ -18,10 +37,18 @@ const LogoLoader = () => {
 
     return (
       <div className='logoParent'>
-        <p style={{ opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s' }}>a</p>
-        <p style={{ opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s 1s' }}>b</p>
-        <p style={{ opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s 2s' }}>c</p>
-        <p style={{ opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s 3s' }}>struktura</p>
+        <p style={{ opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s' }}>
+        <AdvancedImage cldImg={myImage1} />
+        </p>
+        <p style={{ opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s 1s' }}>
+          <AdvancedImage cldImg={myImage2} />
+        </p>
+        <p style={{ opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s 2s' }}>
+          <AdvancedImage cldImg={myImage3} />
+        </p>
+        <p style={{ opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s 3s' }}>
+          <AdvancedImage cldImg={myImage4} />
+        </p>
       </div>
     );
   };
