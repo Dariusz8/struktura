a3925682c944c328b75eafcca5d84a2c43d9a583
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2024-04-17 21:50:34 -0400

    sidebarConditionalSetup

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,13 +1,40 @@
 import './assets/HomePage.css';
-import { signal } from "@preact/signals";
+import { signal, effect } from "@preact/signals";
+import { Cloudinary } from '@cloudinary/url-gen';
+import {AdvancedImage} from '@cloudinary/react';
 
 const HomePage = () => {
+    //create and configure to my cloudinary instance
+    const cld = new Cloudinary({
+        cloud: {
+          cloudName: 'dly85se71'
+        }
+      });
     
     const allPictures = signal([
         {
-            name:"test",
+            name:"test1",
             tags:"house",
-            info:"this is a test"
+            info:"this is a test1 pre",
+            condition:"pre"
+    },
+    {
+        name:"test1",
+        tags:"house",
+        info:"this is a test1 post",
+        condition:"post"
+    },
+    {
+        name:"test2",
+        tags:"house",
+        info:"this is a test2 pre",
+        condition:"pre"
+    },
+    {
+        name:"test2",
+        tags:"house",
+        info:"this is a test2 post",
+        condition:"post"
     }
     ])
     return (
--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -6,37 +6,41 @@ import { PiTelevisionSimpleBold } from "react-icons/pi";
 import { LiaBedSolid } from "react-icons/lia";
 import { PiToiletBold } from "react-icons/pi";
 import { MdOutlineKitchen } from "react-icons/md";
-import { signal } from "@preact/signals"; 
+import { signal, useSignal, effect } from "@preact/signals-react"; 
 
 const SideBar = () => {
-    //create and configure to my cloudinary instance
-  const cld = new Cloudinary({
-    cloud: {
-      cloudName: 'dly85se71'
-    }
-  });
-  //cloudinary pictures being taken
-  const logoImg = cld.image('STRUKTURA');
-  const sideBarMenu = cld.image('STRUKTURA1');
+
+    const sideBarStatus = signal(false);
+//create and configure to my cloudinary instance
+    const cld = new Cloudinary({
+        cloud: {
+        cloudName: 'dly85se71'
+        }
+    });
+//cloudinary pictures being taken
+    const logoImg = cld.image('STRUKTURA');
+    const sideBarMenu = cld.image('STRUKTURA1');
 
   //use cloudinary sdk to have image in ready format
-  logoImg.format('auto')
-    .quality('auto')
-    .resize(scale().width(0.10).regionRelative());
+    logoImg.format('auto')
+        .quality('auto')
+        .resize(scale().width(0.10).regionRelative());
 
-  sideBarMenu.format('auto')
-    .quality('auto')
-    .resize(scale().width(0.065).regionRelative());
+    sideBarMenu.format('auto')
+        .quality('auto')
+        .resize(scale().width(0.065).regionRelative());
     ;
 
     return (
+        <div>
+        {sideBarStatus && 
         <div className='sideBarBoxDaddy'>
             <p className='sideBarLogo'>
                 <AdvancedImage cldImg={logoImg} alt="Small Struktura Company Logo" />
             </p>
-            <p className='sideBarMenuButton'>
+            <button className='sideBarMenuButtonOn'>
                 <AdvancedImage cldImg={sideBarMenu} alt="Arrow Button For Sidebar Menu" />
-            </p>
+            </button>
             <button className='kitchenIcon sideBarOptions' title="Kitchen">
                 <MdOutlineKitchen />
                 <span>Kitchen</span>
@@ -54,6 +58,18 @@ const SideBar = () => {
                 <span>TV</span>
             </button>
         </div>
+        }
+        {!sideBarStatus &&
+            <div className='sideBarBoxDaddy'>
+                <p className='sideBarLogo'>
+                    <AdvancedImage cldImg={logoImg} alt="Small Struktura Company Logo" />
+                </p>
+            <button className='sideBarMenuButtonOff'>
+                <AdvancedImage cldImg={sideBarMenu} alt="Arrow Button For Sidebar Menu" />
+            </button>
+            </div>
+        }
+    </div>
     )
 }
 
