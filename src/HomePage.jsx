5dfbf7f70d7a5421027e4c1efb5fa7460f7763ba
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2024-05-07 21:00:28 -0400

    beforeAfterClick

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,20 +4,21 @@ import React, {useState} from 'react';
 import { Cloudinary } from '@cloudinary/url-gen';
 import {AdvancedImage} from '@cloudinary/react';
 
-const cld = new Cloudinary({
-    cloud: {
-      cloudName: 'dly85se71'
-    }
-  });
-
-  const testImg1 = cld.image('cld-sample-5');
-  const testImg2 = cld.image('cld-sample-3');
-  const testImg3 = cld.image('cld-sample-4');
-  const testImg4 = cld.image('cld-sample-2');
-  const testImg5 = cld.image('cld-sample');
-
 const HomePage = () => {
+    const cld = new Cloudinary({
+        cloud: {
+          cloudName: 'dly85se71'
+        }
+      });
+    
+      const testImg1 = cld.image('cld-sample-5');
+      const testImg2 = cld.image('cld-sample-3');
+      const testImg3 = cld.image('cld-sample-4');
+      const testImg4 = cld.image('cld-sample-2');
+      const testImg5 = cld.image('cld-sample');
     //create and configure to my cloudinary instance
+    const [before, setBefore] = useState(true);
+    const [after,setAfter] = useState(false);
     const [pictures,setPictures] = useState({
         entry1:{
             pre: testImg1,
@@ -32,11 +33,16 @@ const HomePage = () => {
             info: "Texas"
         },
     })
-
-    return (
+    //FUNCTIONS
+    const screenClick = () => {
+        setBefore(!before)
+        setAfter(!after)
+    }
+    //CODE TO EXPORT AFTER
+    const showingBeforePictures = (
         <div>
             <input type="text" className="searchBar" placeholder="Search..." />
-            <div className='homeBoxDaddy'>
+            <div className='homeBoxDaddy' onClick={screenClick}>
                 {pictures && typeof pictures === 'object' && Object.values(pictures).map((entry, index) => (
                     <div key={index}>
                         <AdvancedImage cldImg={entry.pre} />
@@ -46,6 +52,32 @@ const HomePage = () => {
                     </div>
                 ))}
             </div>
+        </div>)
+    
+    const showingAfterPictures = (
+        <div>
+            <input type="text" className="searchBar" placeholder="Search..." />
+            <div className='homeBoxDaddy'>
+                {pictures && typeof pictures === 'object' && Object.values(pictures).map((entry, index) => (
+                    <div key={index}>
+                        <AdvancedImage cldImg={entry.post} />
+                        <p>
+                            {entry.info}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>)
+
+    
+    return (
+        <div>
+        {
+            before && showingBeforePictures
+        }
+        {
+            after && showingAfterPictures
+        }
         </div>
     )
 }
